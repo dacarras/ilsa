568ee744263fdcd9925d16f0c6a558885e7e40ad
dacarras <dacarras@gmail.com>
2022-10-06 10:10:41 +0200

    add iccs 2016 dara

---
+++ R/iccs_2016.R
@@ -0,0 +1,20 @@
+#' ICCS 2016
+#'
+#' Merge of students data from the International Civic and Citizenship Education Study 2016 (ICCS 2016).
+#' The study contains representative samples of 8th graders (14 years of age), from 24 different educational systems.
+#'
+#' @docType data
+#'
+#' @usage data(iccs_2016)
+#'
+#' @format Object class \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Schulz, W., Carstens, R., Losito, B., & Fraillon, J. (2018). ICCS 2016 Technical Report (W. Schulz, R. Carstens, B. Losito, & J. Fraillon (eds.)). International Association for the Evaluation of Educational Achievement (IEA).
+#'
+#' @source IEA, <https://www.iea.nl/index.php/data-tools/repository/iccs>
+#'
+#' @examples
+#' dplyr::glimpse(ilsa::iccs_2016)
+"iccs_2016"
