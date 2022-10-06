0079461d2607e66e3757c41250ec75ed85df2892
dacarras <dacarras@gmail.com>
2022-10-06 10:27:59 +0200

    add iccs 2009 and update readme

---
+++ R/iccs_2009.R
@@ -0,0 +1,20 @@
+#' ICCS 2009
+#'
+#' Merge of students data from the International Civic and Citizenship Education Study 2009 (ICCS 2009).
+#' The study contains representative samples of 8th graders (14 years of age), from 38 different educational systems.
+#'
+#' @docType data
+#'
+#' @usage data(iccs_2009)
+#'
+#' @format Object class \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Schulz, W., Ainley, J., & Fraillon, J. (2011). ICCS 2009 Technical Report (W. Schulz, J. Ainley, & J. Fraillon (eds.)). International Association for the Evaluation of Educational Achievement (IEA). http://www.iea.nl/fileadmin/user_upload/Publications/Electronic_versions/ICCS_2009_Technical_Report.pdf
+#'
+#' @source IEA, <https://www.iea.nl/index.php/data-tools/repository/iccs>
+#'
+#' @examples
+#' dplyr::glimpse(ilsa::iccs_2009)
+"iccs_2009"
